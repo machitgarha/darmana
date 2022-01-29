3ef9aed8062814b811d1e07eac433d0c2ea7bc0a
MohamadDini <mohamad.dini.1278@gmail.com>
2022-01-29 17:06:50 +0330

    create treatments table

---
+++ laravel/database/migrations/2022_01_29_132725_create_treatment_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTreatmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->string('name of treatment',25) ;
+            $table->integer('snot')->unsigned()->unique()->primary() ; //--> snot=seriyal number of treatment
+            $table->dateTime('date of treatment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('treatments');
+    }
+}
